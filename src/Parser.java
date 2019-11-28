780e0fc275a98afbbe0b135841a39fcd7eddb425
Jack Chen <u6667796@anu.edu.au>
2019-11-29 00:37:54 +1100

    rewrite

---
+++ src/Parser.java
@@ -0,0 +1,9 @@
+public class Parser {
+    String s;
+    Token t;
+
+    public Token parse(String s){
+
+        return t;
+    }
+}
---
+++ src/Token.java
@@ -0,0 +1,6 @@
+public abstract class Token {
+    /*
+    Paragraph = Sentence ; Paragraph
+    Sentence  = Task , Score | Task , Score (Comment)
+    */
+}
